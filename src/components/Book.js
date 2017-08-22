1e9a5bec2376072844605382b5946538dd2bd59a
Christian Townsdin <ctownsdin@expedia.org>
2017-08-21 20:10:32 -0700

    render a book using mock data and prop types are working - shows a warning when not provided

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,14 @@
 import React from 'react'
-// import PropTypes from 'prop-types'  // TODO:
+import PropTypes from 'prop-types'
 
 // <Book /> - a presentational "leaf" component that displays a book
-const Book = () => (
+const Book = (props) => (
   <div className='book'>
     <div className='book-top'>
-      <div className='book-cover' style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }} />
+      <div className='book-cover' style={{ 
+        width: 128, height: 192, 
+        backgroundImage: `url(${props.bookData.imageLinks.smallThumbnail})`
+      }} />
       <div className='book-shelf-changer'>
         <select>
           <option value='none' disabled>Move to...</option>
@@ -16,11 +19,13 @@ const Book = () => (
         </select>
       </div>
     </div>
-    <div className='book-title'>To Kill a Mockingbird</div>
-    <div className='book-authors'>Harper Lee</div>
+    <div className='book-title'>{props.bookData.title}</div>
+    <div className='book-authors'>{props.bookData.authors[0]}}</div>
   </div>
 )
 
 export default Book
 
-// Book.propTypes = { }  TODO:
+Book.propTypes = {
+  bookData: PropTypes.object.isRequired
+}
